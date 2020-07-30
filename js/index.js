016bad114bc395e9730acccd3f3cda161909ecb9
eunji7 <68845409+eunji7@users.noreply.github.com>
2020-07-30 12:28:54 +0900

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,24 @@
+$('#c1').click(function () {
+    $('#c1').fadeOut();
+    $('.child').append('<p>　</p><p>もう一回挑戦しましょう！</p>');
+});
+
+$('#c2').click(function () {
+    $('#c2').fadeOut();
+    $('.child').append('<p>　</p><p>もう一回挑戦しましょう！</p>');
+});
+
+$('#c3').click(function () {
+    $('#c3').fadeOut();
+    $('.child').append('<p>　</p><p>もう一回挑戦しましょう！</p>');
+});
+
+$('#c5').click(function () {
+    $('#c5').fadeOut();
+    $('.child').append('<p>　</p><p>もう一回挑戦しましょう！</p>');
+});
+
+$('#c4').click(function () {
+    $('.child').append('<p>　</p><p>正解です！世界中の子供の6人に1人は教育を受けることなく、大人になっています。</p><p>簡単なアンケートに答えるだけで、教育を受けられていない子供のために無料で支援することができるサイトがあります。</p><p><a href="https://gooddo.jp/nf/questionpart-eedu-201912-nyo-ver2/?from=gooddo_mg_202004" target="_blank">こちら</a>をクリックして簡単なアンケートで支援しましょう！</p>');
+});
+
